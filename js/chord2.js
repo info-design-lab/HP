3ec40f9267c9d1070ca288439c71b376b4360a04
eeshani <eeshani.m@gmail.com>
2019-12-12 12:26:26 +0530

    Add files via upload

--- js/chord2.js
+++ js/chord2.js
@@ -1,12 +1,10 @@
 queue()
-    .defer(d3.csv, 'data/Harry.csv')
-    .defer(d3.csv, 'data/Ron.csv')
-    .defer(d3.csv, 'data/Hermoini.csv')
-    .defer(d3.csv, 'harrysankey.csv')
+    .defer(d3.csv, 'data/Harry_modified.csv')
+    .defer(d3.csv, 'data/Ron_modified.csv')
+    .defer(d3.csv, 'data/Hermoini_modified.csv')
     .await(makeRankVis);
 
-function makeRankVis(error, harry, ron, hermoini,harrysankey){
-
+function makeRankVis(error, harry, ron, hermoini){
     if(error){
         console.log(error);
     }
@@ -143,6 +141,7 @@ function makeVis(current_movie){
 
             d3.selectAll("path")
               .style("opacity", 0.7);
+            console.log(this.getAttribute("class"));
           });
         
     // Add the links between groups
@@ -180,132 +179,5 @@ function makeVis(current_movie){
               .style("opacity", 0.7);
           });
         }
-
-/**
-var units = "Widgets";
-
-   var margin = {top: 10, right: 10, bottom: 10, left: 10},
-    width = 700 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom;
-
-    var formatNumber = d3.format(",.0f"),    // zero decimal places
-    format = function(d) { return formatNumber(d) + " " + units; };
-
-    // append the svg canvas to the page
-   var svg2 = d3.select("#sankey_diagram").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-      .attr("transform", 
-          "translate(" + margin.left + "," + margin.top + ")");
-   
-  // Set the sankey diagram properties
-  var sankey = d3.sankey()
-      .nodeWidth(36)
-      .nodePadding(40)
-      .size([width, height]);
-
-  var path = sankey.links();
-
-  // load the data (using the timelyportfolio csv method)
-d3.csv("Harry-Sankey.csv", function(error, data) {
-
-  //set up graph in same style as original example but empty
-  graph = {"nodes" : [], "links" : []};
-
-    data.forEach(function (d) {
-      graph.nodes.push({ "name": d.Question });
-      graph.nodes.push({ "name": d.Movie });
-      graph.links.push({ "source": d.Question,
-                         "target": d.Movie,
-                         "value": +d.Value });
-     });
-
-     // return only the distinct / unique nodes
-     graph.nodes = d3.keys(d3.nest()
-       .key(function (d) { 
-      return d.name; })
-       .map(graph.nodes));
-
-     // loop through each link replacing the text with its index from node
-     graph.links.forEach(function (d, i) {
-       graph.links[i].source = graph.nodes.indexOf(graph.links[i].source);
-       graph.links[i].target = graph.nodes.indexOf(graph.links[i].target);
-     });
-
-     //now loop through each nodes to make nodes an array of objects
-     // rather than an array of strings
-     graph.nodes.forEach(function (d, i) {
-       graph.nodes[i] = { "name": d };
-     });
-
- 
-  sankey
-    .nodes(graph.nodes)
-    .links(graph.links);
-
-// add in the links
-  var link = svg2.append("g").selectAll(".link")
-      .data(graph.links)
-    .enter().append("path")
-      .attr("class", "link")
-      .attr("d", path)
-      .style("stroke-width", function(d) { return Math.max(1, d.dy); })
-      .sort(function(a, b) { return b.dy - a.dy; });
-
-// add the link titles
-  link.append("title")
-        .text(function(d) {
-          //console.log(d);
-        return d.Question + " → " + 
-                d.Movie + "\n" + format(d.Value); });
-
-// add in the nodes
-  var node = svg2.append("g").selectAll(".node")
-      .data(graph.nodes)
-    .enter().append("g")
-      .attr("class", "node")
-      .attr("transform", function(d) { 
-      return "translate(" + d.x + "," + d.y + ")"; })
-    .call(d3.behavior.drag()
-      .origin(function(d) { return d; })
-      .on("dragstart", function() { 
-      this.parentNode.appendChild(this); })
-      .on("drag", dragmove));
-
-// add the rectangles for the nodes
-  node.append("rect")
-      .attr("height", function(d) { return d.dy; })
-      .attr("width", sankey.nodeWidth())
-      .style("fill", function(d) { 
-      return d.color = color(d.name.replace(/ ./, "")); })
-      .style("stroke", function(d) { 
-      return d3.rgb(d.color).darker(2); })
-    .append("title")
-      .text(function(d) { 
-      return d.name + "\n" + format(d.value); });
-
-// add in the title for the nodes
-  node.append("text")
-      .attr("x", -6)
-      .attr("y", function(d) { return d.dy / 2; })
-      .attr("dy", ".35em")
-      .attr("text-anchor", "end")
-      .attr("transform", null)
-      .text(function(d) { return d.name; })
-    .filter(function(d) { return d.x < width / 2; })
-      .attr("x", 6 + sankey.nodeWidth())
-      .attr("text-anchor", "start");
-
-// the function for moving the nodes
-  function dragmove(d) {
-    d3.select(this).attr("transform", 
-        "translate(" + d.x + "," + (
-                d.y = Math.max(0, Math.min(height - d.dy, d3.event.y))
-            ) + ")");
-    sankey.relayout();
-    link.attr("d", path);
-  }
-}); **/
 
 }
